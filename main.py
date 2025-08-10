78e0868a282fba2f15058f5c309aee8f6bebdd61
devilking667 <86866770+devilking667@users.noreply.github.com>
2025-08-10 09:09:00 +0500

    Update main.py

--- main.py
+++ main.py
@@ -252,7 +252,7 @@ def bot_logs(bot_id):
 
 
 
-
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
+
